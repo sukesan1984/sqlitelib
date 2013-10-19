1f6a744f23b2f2cbeb7b6ed822489a74869e46f6
Kosuke Takami <takami.kosuke@dena.com>
2013-10-19 14:51:04 +0900

    Initial Commit

---
+++ sqltest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sqltest
+//
+//  Created by Kosuke Takami on 2013/10/19.
+//  Copyright (c) 2013年 Kosuke Takami. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
